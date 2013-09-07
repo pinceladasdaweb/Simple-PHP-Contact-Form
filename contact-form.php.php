de093ce811de7ff1ef877432bdbe9ea1eddf074b
Pedro Rogério <pinceladasdaweb@hotmail.com>
2013-09-06 22:27:06 -0300

    Initial commit

---
+++ contact-form.php.php
@@ -0,0 +1,122 @@
+<?php
+$emailTo = '<YOUR_EMAIL_HERE>';
+
+if($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if(trim($_POST['name']) === '') {
+        $hasError = true;
+    } else {
+        $name = trim($_POST['name']);
+    }
+
+    if(trim($_POST['email']) === '')  {
+        $hasError = true;
+    } else if (!preg_match("/^[^0-9][A-z0-9_]+([.][A-z0-9_]+)*[@][A-z0-9_]+([.][A-z0-9_]+)*[.][A-z]{2,4}$/", trim($_POST['email']))) {
+        $hasError = true;
+    } else {
+        $email = trim($_POST['email']);
+    }
+
+    if(trim($_POST['assunto']) === '') {
+        $hasError = true;
+    } else {
+        $assunto = trim($_POST['assunto']);
+    }
+
+    if(trim($_POST['mensagem']) === '') {
+        $hasError = true;
+    } else {
+        if(function_exists('stripslashes')) {
+            $mensagem = stripslashes(trim($_POST['mensagem']));
+        } else {
+            $mensagem = trim($_POST['mensagem']);
+        }
+    }
+
+    if(!isset($hasError)) {
+        $subject = '[Contato via site] '.$assunto;
+        $body = "Nome: $name <br> Email: $email <br> Mensagem: $mensagem";
+
+        $headers  = 'MIME-Version: 1.1'. PHP_EOL;
+        $headers .= 'Content-type: text/html; charset=utf-8'. PHP_EOL;
+        $headers .= 'From: '.$name.' <'.$email.'>' . PHP_EOL;
+        $headers .= 'Return-Path: ' . $emailTo . PHP_EOL;
+        $headers .= 'Reply-To: ' . $email . PHP_EOL;
+
+        mail($emailTo, $subject, $body, $headers);
+        $emailSent = true;
+    }
+}
+?><!DOCTYPE html>
+<html>
+<head>
+    <title>Simple PHP Contact Form</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" media="screen">
+</head>
+<body>
+    <div class="jumbotron">
+        <h1>Simple PHP Contact Form</h1>
+        <p>A Simple Contact Form developed in PHP with HTML5 Form validation. Has a fallback in jQuery for browsers that do not support HTML5 form validation.</p>
+    </div>
+    <?php if(isset($emailSent) && $emailSent === true) { ?>
+        <div class="col-md-6 col-md-offset-3">
+            <div class="alert alert-success text-center">Sua mensagem foi enviada com sucesso.</div>
+        </div>
+    <?php
+    } else {
+        if(isset($hasError)) {
+    ?>
+        <div class="col-md-5 col-md-offset-4">
+            <div class="alert alert-danger text-center">Houve um erro no envio, tente novamente mais tarde.</div>
+        </div>
+    <?php } ?>
+
+    <div class="col-md-6 col-md-offset-3">
+        <form action="<?php echo $_SERVER['REQUEST_URI']; ?>" id="contact-form" class="form-horizontal" role="form" method="post">
+            <div class="form-group">
+                <label for="name" class="col-lg-2 control-label">Nome</label>
+                <div class="col-lg-10">
+                    <input type="text" class="form-control" id="name" name="name" placeholder="Nome" required>
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="email" class="col-lg-2 control-label">Email</label>
+                <div class="col-lg-10">
+                    <input type="email" class="form-control" id="email" name="email" placeholder="Email" required>
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="assunto" class="col-lg-2 control-label">Assunto</label>
+                <div class="col-lg-10">
+                    <input type="text" class="form-control" id="assunto" name="assunto" placeholder="Assunto" required>
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="mensagem" class="col-lg-2 control-label">Mensagem</label>
+                <div class="col-lg-10">
+                    <textarea class="form-control" rows="3" id="mensagem" name="mensagem" placeholder="Mensagem" required><?php if(isset($_POST['mensagem'])) { if(function_exists('stripslashes')) { echo stripslashes($_POST['mensagem']); } else { echo $_POST['mensagem']; } } ?></textarea>
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="col-lg-offset-2 col-lg-10">
+                    <button type="submit" class="btn btn-default">Enviar</button>
+                </div>
+            </div>
+        </form>
+    </div>
+    <?php } ?>
+
+    <?php
+        function IEVersion() {
+            return preg_match('/MSIE (.*?);/', $_SERVER['HTTP_USER_AGENT'], $matches) ? floatval($matches[1]) : null;
+        }
+
+        if(IEVersion() < 9 && IEVersion() != null) {
+            echo "<script type='text/javascript' src='//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js'></script>". PHP_EOL;
+        } else {
+            echo "<script type='text/javascript' src='//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js'></script>". PHP_EOL;
+        }
+    ?>
+    <script type="text/javascript" src="assets/js/contact-form.js"></script>
+</body>
+</html>
