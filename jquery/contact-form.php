6bfc7e62802f35c5d346849e747a6b6df3f50b81
Pedro Rogério <pinceladasdaweb@hotmail.com>
2015-10-25 12:23:07 -0200

    Adding new headers to prevent the email is marked as spam. Closes issue #11

--- jquery/contact-form.php
+++ jquery/contact-form.php
@@ -20,14 +20,18 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
         $subject = "$subjectPrefix $subject";
         $body = "Nome: $name <br /> Email: $email <br /> Telefone: $phone <br /> Mensagem: $message";
 
-        $headers  = 'MIME-Version: 1.1' . PHP_EOL;
-        $headers .= 'Content-type: text/html; charset=utf-8' . PHP_EOL;
-        $headers .= "From: $name <$email>" . PHP_EOL;
+        $headers  = "MIME-Version: 1.1" . PHP_EOL;
+        $headers .= "Content-type: text/html; charset=utf-8" . PHP_EOL;
+        $headers .= "Content-Transfer-Encoding: 8bit" . PHP_EOL;
+        $headers .= "Date: " . date('r', $_SERVER['REQUEST_TIME']) . PHP_EOL;
+        $headers .= "Message-ID: <" . $_SERVER['REQUEST_TIME'] . md5($_SERVER['REQUEST_TIME']) . '@' . $_SERVER['SERVER_NAME'] . '>' . PHP_EOL;
+        $headers .= "From: " . "=?UTF-8?B?".base64_encode($name)."?=" . "<$email>" . PHP_EOL;
         $headers .= "Return-Path: $emailTo" . PHP_EOL;
         $headers .= "Reply-To: $email" . PHP_EOL;
         $headers .= "X-Mailer: PHP/". phpversion() . PHP_EOL;
+        $headers .= "X-Originating-IP: " . $_SERVER['SERVER_ADDR'] . PHP_EOL;
 
-        mail($emailTo, $subject, $body, $headers);
+        mail($emailTo, "=?utf-8?B?".base64_encode($subject)."?=", $body, $headers);
         $emailSent = true;
     } else {
         $hasError = true;
