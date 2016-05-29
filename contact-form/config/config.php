c2ed9bd55747024b306d76b5997ba5320c75e8f1
Pedro Rogério <pinceladasdaweb@hotmail.com>
2016-05-28 21:56:43 -0300

    Added a config file (#10)

---
+++ contact-form/config/config.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'messages' => [
+        'error'   => 'There was an error sending, please try again later.',
+        'success' => 'Your message has been sent successfully.'
+    ],
+    'fields' => [
+        'name'     => 'Name',
+        'email'    => 'Email',
+        'phone'    => 'Phone',
+        'subject'  => 'Subject',
+        'message'  => 'Message',
+        'btn-send' => 'Send'
+    ]
+];
