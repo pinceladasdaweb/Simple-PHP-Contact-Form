b3453d42ed9a31cd37066717ec4e3ae59392e4cb
Pedro Rogério <pinceladasdaweb@hotmail.com>
2016-05-28 23:58:47 -0300

    Update config

--- contact-form/config/config.php
+++ contact-form/config/config.php
@@ -1,6 +1,13 @@
 <?php
 
 return [
+    'subject' => [
+        'prefix' => '[Contact Form]'
+    ],
+    'emails' => [
+        'to'   => '',
+        'from' => ''
+    ],
     'messages' => [
         'error'   => 'There was an error sending, please try again later.',
         'success' => 'Your message has been sent successfully.'
